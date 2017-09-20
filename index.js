138ce24922103f6a193db1237fadcd67ee817cfa
Peizan Wang <peizan.wang@mulesoft.com>
2017-09-20 16:13:18 -0700

    Init commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const loggerConfig = require('./lib/util/log_config');
+const logger = require('winston');
+
+const compression  = require('compression');
+const bodyParser = require('body-parser');
+
+const error = require('./lib/error/error');
+const requestLogMiddleware = require('./lib/middleware/request_log_middleware');
+const responseMiddleware = require('./lib/middleware/response_middleware');
+const router = require('./lib/router/router');
+
+require('./lib/util/logger')(loggerConfig);
+
+class Waffle extends router {
+
+  constructor(config) {
+    super();
+
+    this.config = config || {};
+
+    app.use(compression());
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(requestLogMiddleware);
+  }
+
+  run(port) {
+    port = port || this.config.port || 8080;
+
+    app.use(this.router);
+
+    app.use(responseMiddleware.respond);
+    app.use(responseMiddleware.respondError);
+
+    app.listen(port, function () {
+      logger.notice(`[waffle] Server running on port ${port} ...`);
+    });
+  }
+}
+
+Waffle.error = error;
+Waffle.router = router;
+
+module.exports = Waffle;
---
+++ test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Promise = require('bluebird');
+
+const WaffleModule = require('./index');
+const waffle = new WaffleModule();
+
+waffle.get('/hi', function (req, res) {
+  return Promise.resolve({ hello: 'world' });
+});
+
+waffle.get('/echo/:name', function (req, res) {
+  return Promise.resolve(req.params.name);
+});
+
+const v1Router = new WaffleModule.router();
+v1Router.get('/echo/:job', function (req, res) {
+  return Promise.resolve({ job: req.params.job });
+});
+
+const statusRouter = new WaffleModule.router();
+statusRouter.get('/health', function (req, res) {
+  return Promise.resolve("ok");
+});
+
+v1Router.use('/status', statusRouter);
+waffle.use('/v1', v1Router);
+
+waffle.run();
